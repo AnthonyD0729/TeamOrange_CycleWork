c4828a1348a2ff41fefe7515bc8ed0f325433f7e
AnthonyD0729 <103949916+AnthonyD0729@users.noreply.github.com>
2022-09-28 19:28:55 -0400

    I thought I tried something

--- Cycle 1/colorbutton.py
+++ Cycle 1/colorbutton.py
@@ -6,29 +6,18 @@ top = Tk()
 top.geometry("200x100")  
   
 def red():  
-    messagebox.showinfo("Hello", "Red Button clicked")  
+    messagebox.showinfo("Hello", "Start Button clicked")  
 def blue():  
-    messagebox.showinfo("Hello", "Blue Button clicked")  
+    messagebox.showinfo("Hello", "Quit Button clicked")  
 def green():  
     messagebox.showinfo("Hello", "Green Button clicked")  
 def yellow():  
     messagebox.showinfo("Hello", "Yellow Button clicked")  
   
   
-b1 = Button(top,text = "Red",command = red,activeforeground = "red",activebackground = "black",pady=10)  
-  
-b2 = Button(top, text = "Blue",command = blue,activeforeground = "blue",activebackground = "black",pady=10)  
-  
-b3 = Button(top, text = "Green",command = green, activeforeground = "green",activebackground = "black",pady = 10)  
-  
-b4 = Button(top, text = "Yellow",command = yellow, activeforeground = "yellow",activebackground = "black",padx =10,pady = 10)  
-  
-b1.pack(side = LEFT)  
-  
-b2.pack(side = RIGHT)  
-  
-b3.pack(side = TOP)  
-  
-b4.pack(side = BOTTOM)  
+b1 = Button(text = "Start",command = red,activeforeground = "red",activebackground = "black",pady=10)  
+b1.place(x=0, y=60)  
+b2 = Button(text = "Quit",command = blue,activeforeground = "blue",activebackground = "black",pady=10)  
+b2.place(x=165, y=60)
   
 top.mainloop()  
