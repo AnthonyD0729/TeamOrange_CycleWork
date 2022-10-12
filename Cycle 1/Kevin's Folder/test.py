9f0bb4c157867cd5c63d155d84c265c341a65ba3
musclezmaloneJR <113373784+musclezmaloneJR@users.noreply.github.com>
2022-10-12 13:29:19 -0400

    i am 100% going to close this window

---
+++ Cycle 1/Kevin's Folder/test.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+pygame.init()
+
+xUnits = 600
+yUnits = 600
+screen = pygame.display.set_mode([xUnits, yUnits], 0, 32)  #Assign the dimension of screen
+clock = pygame.time.Clock()
+clock.tick(60)              #Cap the frames per second [fps] to 60 fps
+
+loopCon = True
+while loopCon:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           loopCon = False
+
+    screen.fill((74, 65, 42))   #Refill the screen with the background after the square has moved
+    pygame.draw.line(screen, [255, 0, 0], [0, 0], [xUnits/2, yUnits/2], 1)
+    pygame.display.flip()       #Update the entire display surface to the computer screen
+#screen.blit()
