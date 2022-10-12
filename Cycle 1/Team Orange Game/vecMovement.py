ac5c41b9215f1eee17b5a39903de2bb921c723a0
Isaac <beancollector01@gmail.com>
2022-10-12 13:48:46 -0400

    rect

---
+++ Cycle 1/Team Orange Game/vecMovement.py
@@ -0,0 +1,14 @@
+expiration = False
+iAmGoingToLoseIt = True
+
+if iAmGoingToLoseIt:
+    expiration = True
+    print("I have perished due to 'rect' being the single worst piece of garbage ever conceived.")
+    print("How could something so absolutely vile exist without an endlessly malevolent creator?")
+    print("The implications of such a being spell doom for the human race,")
+    print("for they seek only to cause suffering as far as their arms can reach.")
+    print("The only remedy for my fellow man is to accept imminent demise...")
+    print("...or to end it all before the suffering begins.")
+    print("Recall my words when the End begins.")
+
+
