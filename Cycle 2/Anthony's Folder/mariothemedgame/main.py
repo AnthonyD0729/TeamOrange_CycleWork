f21d2ddba10db56848a13d8e7f1e6ae7819c66f0
Anthony Deyoe <deyoea@sunypoly.edu>
2022-10-24 19:46:56 -0400

    check it out if you dare

--- Cycle 2/Anthony's Folder/mariothemedgame/main.py
+++ Cycle 2/Anthony's Folder/mariothemedgame/main.py
@@ -1,5 +1,29 @@
 import sys
 import pygame, sys
-
+from settings import *
 #Pygame Setup
 pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill("black")
+
+    pygame.display.update()
+    clock.tick(60)
+
+
+
+
+
+
+
+
+
+
+
+
