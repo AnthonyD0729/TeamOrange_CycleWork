caaa9bc766ab461674d8b21450b7cb6c89417365
Sal Mecca <meccas@sunypoly.edu>
2022-10-24 13:31:11 -0400

    Some changes to ATLock

--- Cycle 1/Sal's Folder/ATLock.py
+++ Cycle 1/Sal's Folder/ATLock.py
@@ -1,7 +1,7 @@
 #New ATLock File
 import sys
 import random
-import os 
+import os.path 
 from datetime import date
 
 lock_type = 3
@@ -28,12 +28,14 @@ def encode(s):
         for i in len(s):
             lock_pos += 1
             if(lock_pos > len(lock_code)):
-                lock_pos = 0
-            if(s[i] < " " or s[i] > '~'):  #Not exactly correct but those represent the printable ASCII lowest and highest characters
+                lock_pos = 1
+            #if(s[i] < " " or s[i] > '~'):  #Not exactly correct but those represent the printable ASCII lowest and highest characters\
+            if(ord(s[i]) < 31 or ord(s[i]) > 128):
                 s[i] = " "
 
-            this_dat = s[i] & 15
-            s[i] = s[i] ^ (lock_code[lock_pos] ^ lock_dat) + 1 
+            this_dat = ord(s[i]) & 15
+            #s[i] = s[i] ^ (lock_code[lock_pos] ^ lock_dat) + 1 
+            s[i] = chr((ord(s[i]) ^ (ord(lock_code[lock_pos]) ^ lock_dat)) + 1)
             lock_dat = this_dat
         encode = s          
     
@@ -41,11 +43,13 @@ def prepare(s, s1):
     i, j, k, l = 0 #Ints
     s2 #string
     if(len(s1) == 0 or s1[1] == ';'):  #Remove comments
-        s1 = ' '
+        s1 = " "
     else:
         k = 0
+        var = 1
+        step = -1
         i = len(s1)
-        for i in k:
+        for i in range(i, var, step):
             if(s1[i] == ';'):
                 k = i
         if(k > 0):
@@ -69,18 +73,19 @@ def write_line(s, s1):
     s = prepare(s, s1)
     if(len(s) > 0):
         s = encode(s)
-        print(f2, s)
+        f2.write(s)
+        #print(f2, s)
     
 def main():
     lock_pos = 0
     lock_dat = 0
     
     if(len(sys.argv) < 1 or len(sys.argv) > 2):  
-        print("Usage: ATRLOCK <robot[.at2]> [locked[.atl]]")     #btrim removes white space in front and back of string
+        f1.write("Usage: ATRLOCK <robot[.at2]> [locked[.atl]]")     #btrim removes white space in front and back of string
     
     fn1 = str.strip(fn1.upper(sys.argv[1]))  
         
-    if(fn1 == os.basename(fn1)):   #base_name grabs the part of a file before the .atr portion
+    if(fn1 == os.fn1.basename(fn1)):   #base_name grabs the part of a file before the .atr portion
         fn1 = fn1 + ".AT2"  
     
     if(fn1 == None):
@@ -90,13 +95,13 @@ def main():
     if(len(sys.argv) == 2):
         fn2 = str.strip(str.upper(sys.argv[2]))
     else:
-        fn2 = os.basename(fn1) + ".ATL"
+        fn2 = os.fn2.basename(fn1) + ".ATL"
         
-    if(fn2 == os.basename(fn2)):
+    if(fn2 == os.fn2.basename(fn2)):
         fn2 = fn2 + ".ATL"
     
     if(fn2 != False):
-        print("Output name ", fn1, " not valid!")
+        fn2.write("Output name ", fn1, " not valid!")
         
     if(fn1 == fn2):
         print("Filenames cannot be the same!")
