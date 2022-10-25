acf1d71bedd82e9a99287a787a8da565387f0abc
Anthony Deyoe <deyoea@sunypoly.edu>
2022-10-24 21:51:16 -0400

    adjusted the screens and added Isaac's program!

--- Cycle 2/Anthony's Folder/mariothemedgame/main.py
+++ Cycle 2/Anthony's Folder/mariothemedgame/main.py
@@ -1,10 +1,13 @@
 import sys
 import pygame, sys
 from settings import *
+from level import Level
+
 #Pygame Setup
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
 
 while True:
     for event in pygame.event.get():
@@ -12,6 +15,7 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill("black")
+    level.run()
 
     pygame.display.update()
     clock.tick(60)
--- Cycle 2/Anthony's Folder/mariothemedgame/settings.py
+++ Cycle 2/Anthony's Folder/mariothemedgame/settings.py
@@ -13,4 +13,4 @@ level_map = [
 
 tile_size = 64
 screen_width = 1200
-screen_height = len(level_map) * title_size
+screen_height = len(level_map) * tile_size
